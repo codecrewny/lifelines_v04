83c9852b96b39a29b4ba566cfaacf9e009b9aab2
Gustavo <gustavoguimaraes@gmail.com>
2014-02-08 21:56:20 -0500

    add controllers, women_resources

--- public/js/controllers/resourcesCtrl.js
+++ public/js/controllers/resourcesCtrl.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('mean.system')
-	.controller('LifeLinesController', ['$scope', 'Global', function ($scope, Global) { 
-		$scope.global = Global; 
-	}]);
+var womensApp = angular.module('womensApp', []);
+
+womensApp.controller('WomenListCtrl', function($scope, $http){
+  $http.get('women_resources/resources.json').success(function(data) {
+    $scope.women_resources = data;
+  });
+ 
+});
